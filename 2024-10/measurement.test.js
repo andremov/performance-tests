4c2660fd1a88ec3464358b25a62d359e8e235c48
Andres-XL <amovillaw01@gmail.com>
2024-04-12 12:09:24 -0500

    Update measurement.test.js

--- 2024-10/measurement.test.js
+++ 2024-10/measurement.test.js
@@ -16,7 +16,7 @@ describe('problem-01', () => {
     const answer = waterTrap(height);
     const endTime = performance.now();
 
-    performanceData['case-01'] = `${(endTime - startTime) * 1000}ms`;
+    performanceData['case-01'] = `${(endTime - startTime) * 1000} μs`;
 
     expect(answer).toBe(correctAnswer);
   });
@@ -29,7 +29,7 @@ describe('problem-01', () => {
     const answer = waterTrap(height);
     const endTime = performance.now();
 
-    performanceData['case-02'] = `${(endTime - startTime) * 1000}ms`;
+    performanceData['case-02'] = `${(endTime - startTime) * 1000} μs`;
 
     expect(answer).toBe(correctAnswer);
   });
@@ -47,7 +47,7 @@ describe('problem-01', () => {
     const answer = waterTrap(height);
     const endTime = performance.now();
 
-    performanceData['case-03'] = `${(endTime - startTime) * 1000}ms`;
+    performanceData['case-03'] = `${(endTime - startTime) * 1000} μs`;
 
     expect(answer).toBe(correctAnswer);
   });
