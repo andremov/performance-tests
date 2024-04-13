5c6541328bf50cb54ecd5dfe8b7e2606fffc21a0
Sonya Castro <68860233+Sonya-c@users.noreply.github.com>
2024-04-13 02:57:40 +0000

    perf: improve problem 02

--- 2024-10/problem-02.js
+++ 2024-10/problem-02.js
@@ -1,23 +1,46 @@
-
-function isPalindrom (str) {
-    return str === str.split('').reverse().join('');
-}
-
 function longestPalindrome(s) {
-    res = "";
+    // nota: resulta que ya habia resuelto este problema usando c++
+    // ¿como lo resolvi en su momento? solo dios y yo sabiamos
+    // ahora solo dios sabe
+    // so solo lo adapete a .js 
     
-    for (let start = 0; start < s.length; start++) {
-        for (let end = s.length; end > start; end--) {
-            sub = s.substring(start, end);
-            
-            // console.log(sub, isPalindrom(sub));
-            
-            if (isPalindrom(sub) && sub.length > res.length)
-                res = sub;
+    let longest = "", temp = "";
+    
+    // choose the expansion point 
+    for (let i = 0; i < s.length; i++) {
+        temp = s[i];
+        
+        // create the palidrome (odd)
+        let j = 1;
+        while (i + j < s.length && i - j >= 0) {
+            if (s[i - j] != s[i + j]) break;
+
+            temp = s[i - j] + temp + s[i + j];
+            j++;
         }
+
+        if (temp.length > longest.length)
+            longest = temp;
+
+        // even
+        temp = "";
+        if (i + 1 < s.length && s[i] == s[i + 1]) {
+            temp = s[i] + temp + s[i + 1];
+            // cout << temp << endl;
+            let j = 1;
+            while (i + j + 1 < s.length && i - j >= 0) {
+                if (s[i - j] != s[i + j + 1]) break;
+
+                temp = s[i - j] + temp + s[i + j + 1];
+                j++;
+            }
+        }
+
+        if (temp.length > longest.length)
+            longest = temp;
     }
     
-    return res; 
+    return longest; 
 }
 
 module.exports = { longestPalindrome };
