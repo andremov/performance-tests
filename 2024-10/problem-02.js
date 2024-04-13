5c2f57c6569864062587520de7ebade32bf44111
Sonya Castro <68860233+Sonya-c@users.noreply.github.com>
2024-04-13 01:03:56 +0000

    problem 2

--- 2024-10/problem-02.js
+++ 2024-10/problem-02.js
@@ -1,5 +1,23 @@
+
+function isPalindrom (str) {
+    return str === str.split('').reverse().join('');
+}
+
 function longestPalindrome(s) {
-  return '';
+    res = "";
+    
+    for (let start = 0; start < s.length; start++) {
+        for (let end = s.length; end > start; end--) {
+            sub = s.substring(start, end);
+            
+            // console.log(sub, isPalindrom(sub));
+            
+            if (isPalindrom(sub) && sub.length > res.length)
+                res = sub;
+        }
+    }
+    
+    return res; 
 }
 
 module.exports = { longestPalindrome };
