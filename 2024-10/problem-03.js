d2c0f808cc2fee2c488e941426e92e485d20d483
Ehider Andres Guarin Ortega <82425546+Ehiderg@users.noreply.github.com>
2024-04-12 19:51:10 -0500

    Punto 11 realizado

--- 2024-10/problem-03.js
+++ 2024-10/problem-03.js
@@ -1,3 +1,20 @@
 function maxArea(height) {
-  return 0;
+  let maxArea = 0;
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left < right) {
+      const minHeight = Math.min(height[left], height[right]);
+      const width = right - left;
+      const area = minHeight * width;
+      maxArea = Math.max(maxArea, area);
+
+      if (height[left] < height[right]) {
+          left++;
+      } else {
+          right--;
+      }
+  }
+
+  return maxArea;
 }
