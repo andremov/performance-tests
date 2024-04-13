98243576c2dcc70327d465073645849e364ea504
Sonya Castro <68860233+Sonya-c@users.noreply.github.com>
2024-04-13 04:13:13 +0000

    feat: problem-01

--- 2024-10/problem-01.js
+++ 2024-10/problem-01.js
@@ -1,5 +1,38 @@
 function waterTrap(height) {
-  return 1;
+    
+    
+    let max_height = [...height].sort((a, b) => b - a)[0];
+    
+    let water = 0; 
+    
+    for (let h = 1; h < max_height; h++) {
+        
+        let line = "";
+        let rain = false; 
+        let temp_water = 0;
+        
+        for (let i = 0; i < height.length; i++) {
+            if (height[i] < h) {
+                line += "0";
+                if (rain) {
+                    // console.log("I'm singin' in the rain");
+                    temp_water += 1; 
+                }
+                
+            } else {
+                line += "1"
+                if (!rain) {
+                    rain = true; 
+                } else {
+                    water += temp_water; 
+                    temp_water = 0; 
+                }
+            }
+        }
+        
+        // console.log(h, " ", line, " ", water);
+    }
+    return water;
 };
 
 module.exports = { waterTrap };
