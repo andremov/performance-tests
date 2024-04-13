7e70108e77dd8e5020c915fde5dae471c84fa091
Sonya Castro <68860233+Sonya-c@users.noreply.github.com>
2024-04-13 04:53:09 +0000

    feat: problem-03

--- 2024-10/problem-03.js
+++ 2024-10/problem-03.js
@@ -1,3 +1,23 @@
 function maxArea(height) {
-  return 0;
+    let max_area = 0, area; 
+    
+    let i = 0; j = height.length - 1; 
+    
+    while (i != j) {
+        area = (j - i)*Math.min(height[j], height[i]);
+        // console.log(i, j, j - i, Math.min(height[j], height[i]), area);
+        
+        if (area > max_area) {
+            max_area = area; 
+        }
+        
+        if (height[i] < height[j]) {
+            i++;
+        } else {
+            j--;
+        }
+    }
+    return max_area; 
 }
+
+module.exports = { maxArea };
