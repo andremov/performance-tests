671d9004acbe189b2eaee629cba9621cd55d9447
MaycolMD <68664300+MaycolMD@users.noreply.github.com>
2024-04-12 19:57:51 -0500

    2 ex done

--- 2024-10/problem-02.js
+++ 2024-10/problem-02.js
@@ -1,5 +1,33 @@
-function longestPalindrome(s) {
-  return '';
-}
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+var isPalindrome = function(s) {
+
+  if (s == s.split('').reverse().join('')){
+      return true;
+  } else {
+      return false;
+  }
+
+};
+
+var longestPalindrome = function(s) {
+  let word = '';
+  for (let i = 0; i < s.length; i++){
+      for(let j = i; j < s.length; j++){
+          if (isPalindrome(s.substring(i, j))){
+              if (s.substring(i, j).length > word.length){
+                  word = s.substring(i, j);
+              };
+          };
+      };
+  };
+
+  return word
+};
+
+console.log(longestPalindrome("cbbd"))
 
 module.exports = { longestPalindrome };
