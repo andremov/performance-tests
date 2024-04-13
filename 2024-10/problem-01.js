ea3eb6add12b66d81d145dd17b135cb01335fb91
rafamagers <72144260+rafamagers@users.noreply.github.com>
2024-04-12 20:07:39 -0500

    confirmo

--- 2024-10/problem-01.js
+++ 2024-10/problem-01.js
@@ -1,5 +1,19 @@
 function waterTrap(height) {
-  return 1;
+  let izq = 0;
+  let der = height.length - 1;
+  let aguamax = 0;
+  
+  while (izq < der) {
+      let agua = Math.min(height[izq], height[der]) * (der - izq);
+      aguamax = Math.max(aguamax, agua);
+      if (height[izq] < height[der]) {
+          izq++;
+      } else {
+          der--;
+      }
+  }
+  
+  return aguamax;
 };
 
 module.exports = { waterTrap };
