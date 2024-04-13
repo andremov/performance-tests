a6f8ab361dc3be88898a3d5831d4e6a17e72d4ca
nm000 <mendozanatalia0827@gmail.com>
2024-04-13 18:40:31 -0500

    problems1&2&3

--- 2024-10/problem-01.js
+++ 2024-10/problem-01.js
@@ -1,4 +1,22 @@
 function waterTrap(height) {
-};
+    agua = 0
+  
+    for (let index = 0; index < height.length; index++) {
+
+        let mayorIzq = 0
+        for (let index2 = 0; index2 < index; index2++) {
+            mayorIzq = height[index2]>mayorIzq? height[index2] : mayorIzq
+        }
+
+        let mayorDer = 0
+        for (let index3 = index; index3 < height.length; index3++) {
+            mayorDer = height[index3]>mayorDer ? height[index3] : mayorDer
+        }
+        
+        agua += (mayorIzq>height[index] && mayorDer>height[index]) ? Math.min(mayorIzq,mayorDer)-height[index] : 0
+    }
+  
+    return agua
+}
 
 module.exports = { waterTrap };
--- 2024-10/problem-02.js
+++ 2024-10/problem-02.js
@@ -1,28 +1,21 @@
-function invertirString(string) {
-  var stringInvertida = []
-  stringInvertida = string.split('')
-  function sortFunction(value1, value2) {
-      return stringInvertida.indexOf(value2 - value1)
-  }
-  stringInvertida = stringInvertida.sort(sortFunction)
-  //console.log(stringInvertida.join(''))
-  return stringInvertida.join('')
+function isPalindrome(s) {
+  return (s === s.split('').reverse().join(''))
 }
-
 
 function longestPalindrome(s) {
-  var cont  = 0
-  while (invertirString(s) !== s) {
-     if (cont===0) { 
-      s = s.slice(1,s.length) 
-      cont = 1 
-    } else { 
-      s = s.slice(0,s.length-1) 
-      cont = 0 
-    } 
-  }
-  return s;
+  var word = '';
+
+  for (let i = 0; i < s.length; i++){
+      for(let j = i; j < s.length; j++){
+          sTemp = s.substring(i, j)
+          if(isPalindrome(sTemp) && sTemp.length > word.length){
+              word = sTemp
+          }
+      }
+  } 
+
+  return word
 }
 
-console.log(longestPalindrome('cbbd'))
-module.exports = { longestPalindrome };
+
+module.exports = { longestPalindrome }
