8380f8a5cf7d2ee49bcbf7d956fffc88ea5677fb
AnwiiD <78710847+AnwiiD@users.noreply.github.com>
2024-04-12 20:10:36 -0500

    watha

--- 2024-10/problem-01.js
+++ 2024-10/problem-01.js
@@ -4,28 +4,24 @@ function waterTrap(height) {
             return 0;
         }
 
-        let left = new Array(n);
-        let right = new Array(n);
-
-        // Fill left array
-        left[0] = height[0];
+        let one = new Array(n);
+        let two = new Array(n);
+        one[0] = height[0];
         for (let i = 1; i < n; i++) {
-            left[i] = Math.max(left[i - 1], height[i]);
+            one[i] = Math.max(one[i - 1], height[i]);
         }
 
-        // Fill right array
-        right[n - 1] = height[n - 1];
+        two[n - 1] = height[n - 1];
         for (let i = n - 2; i >= 0; i--) {
-            right[i] = Math.max(right[i + 1], height[i]);
+            two[i] = Math.max(two[i + 1], height[i]);
         }
 
-        // Calculate trapped water
-        let trappedWater = 0;
+        let watha = 0;
         for (let i = 0; i < n; i++) {
-            trappedWater += Math.min(left[i], right[i]) - height[i];
+            watha += Math.min(one[i], two[i]) - height[i];
         }
 
-        return trappedWater;
+        return watha;
 };
 
 module.exports = { waterTrap };
