1e65e18dccdc8cd5de466203c9403ae16597f9ec
rafamagers <72144260+rafamagers@users.noreply.github.com>
2024-04-12 20:10:20 -0500

    confirmarrr

--- 2024-10/problem-03.js
+++ 2024-10/problem-03.js
@@ -1,3 +1,17 @@
 function maxArea(height) {
-  return 0;
+  let izq = 0;
+  let der = height.length - 1;
+  let aguamax = 0;
+  
+  while (izq < der) {
+      let agua = Math.min(height[izq], height[der]) * (der - izq);
+      aguamax = Math.max(aguamax, agua);
+      if (height[izq] < height[der]) {
+          izq++;
+      } else {
+          der--;
+      }
+  }
+  
+  return aguamax;
 }
