faf3a30883f0f4db9cb670785846f865fe4ab1fd
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-04-12 20:03:09 -0700

    Agregada función para calcular el área máxima de un contenedor

--- 2024-10/problem-03.js
+++ 2024-10/problem-03.js
@@ -1,3 +1,24 @@
 function maxArea(height) {
-  return 0;
+  let maxArea = 0;
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left < right) {
+    console.log("🚀 ~ maxArea ~ right:", right)
+    console.log("🚀 ~ maxArea ~ left:", left)
+    const minHeight = Math.min(height[left], height[right]);
+   
+    maxArea = Math.max(maxArea, minHeight * (right - left));
+    
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return maxArea;
 }
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7])); // 49
+
