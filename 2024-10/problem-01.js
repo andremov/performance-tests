d5cbf7a3d5896bf7db1da6dc0722b9cac57620e2
nm000 <mendozanatalia0827@gmail.com>
2024-04-14 10:03:28 -0500

    problem1_mejorado

--- 2024-10/problem-01.js
+++ 2024-10/problem-01.js
@@ -3,15 +3,9 @@ function waterTrap(height) {
   
     for (let index = 0; index < height.length; index++) {
 
-        let mayorIzq = 0
-        for (let index2 = 0; index2 < index; index2++) {
-            mayorIzq = height[index2]>mayorIzq? height[index2] : mayorIzq
-        }
+        let mayorIzq = Math.max(...height.slice(0,index))
 
-        let mayorDer = 0
-        for (let index3 = index; index3 < height.length; index3++) {
-            mayorDer = height[index3]>mayorDer ? height[index3] : mayorDer
-        }
+        let mayorDer = Math.max(...height.slice(index,index.length))
         
         agua += (mayorIzq>height[index] && mayorDer>height[index]) ? Math.min(mayorIzq,mayorDer)-height[index] : 0
     }
@@ -19,4 +13,4 @@ function waterTrap(height) {
     return agua
 }
 
-module.exports = { waterTrap };
+module.exports = { waterTrap }
